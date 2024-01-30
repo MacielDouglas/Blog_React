d46f5b350575dca80b235c935d675efd43dc219e
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-01-30 13:42:47 -0300

    Criado signin api rota login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import User from "./../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -30,5 +31,36 @@ export const signup = async (req, res, next) => {
     return res.json({ message: "Inscrito com sucesso!!!" });
   } catch (error) {
     return next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // Verificar campos obrigatórios
+    if (!email || !password) {
+      return next(errorHandler(400, "Todos os campos são necessários."));
+    }
+
+    // Buscar usuário no banco de dados
+    const user = await User.findOne({ email }).select("+password");
+
+    // Verificar se o usuário existe
+    if (!user || !bcryptjs.compareSync(password, user.password)) {
+      return next(errorHandler(401, "Credenciais inválidas."));
+    }
+
+    // Gerar token JWT
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    // Responder com o token e os dados do usuário (sem a senha)
+    const { password: _, ...userWithoutPassword } = user._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(userWithoutPassword);
+  } catch (error) {
+    next(error);
   }
 };

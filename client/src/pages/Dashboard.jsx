98863d214f3eec9d662515c7ed02fae5fd33bc10
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-02-27 10:19:27 -0300

    Adicionado overview para o administrador no dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 
 export default function Dashboard() {
   // Obter a localização atual da rota
@@ -43,6 +44,9 @@ export default function Dashboard() {
 
       {/* comentários  */}
       {tab === "comments" && <DashComments />}
+
+      {/* métricas  */}
+      {tab === "dash" && <DashboardComp />}
     </div>
   );
 }
